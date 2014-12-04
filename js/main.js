5f126d589b589e99283c0e32d8a475a787c2d8fa
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-04 14:46:43 -0600

    Basic Data for ToDo App

--- js/main.js
+++ js/main.js
@@ -8,4 +8,22 @@ var app = angular.module("myApp", []);
 
 app.controller('TodoController', function(){
   var vm = this;
+  vm.tasks = [
+  {
+    name: 'Learn Angular',
+    desc: 'Angular will make so happy',
+    due:  'Today'
+
+  },
+  {
+    name: 'Finish Tic-Tac-Toe',
+    desc: 'A very soothing Exercise',
+    due:  'Monday'
+  },
+  {
+    name: 'Get A Job',
+    desc: 'Bills, Bills... Bills',
+    due:  'Never'
+  }
+  ]
 });
